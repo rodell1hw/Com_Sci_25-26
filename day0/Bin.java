57ad9e696bcbea556b288975cb4106c91023eff1
rodell1hw <rodell1@hwemail.com>
2025-08-28 14:53:56 -0700

    8/28/25

---
+++ day0/Bin.java
@@ -0,0 +1,50 @@
+public class Bin {
+
+    private double heightft = 2.4;
+    private String shape = "rectangular";
+    private boolean canOpen = true;
+    private String contents = "plastic";
+    private int holes = 1;
+    private char initial = 'B';
+
+
+    //this runs when you make a new bin
+    public Bin() {
+        double heightft = 2.4;
+        String shape = "rectangular";
+        boolean canOpen = true;
+        String contents = "plastic";
+        int holes = 1;
+        char initial = 'B';
+    }
+    
+
+    public void rename(){
+        initial = 'T';
+        System.out.print("You renamed the bin");
+        System.out.print(initial);
+
+
+    }
+
+    public void empty(){
+        contents = "nothing";
+        System.out.print("You emptied the bin");
+        System.out.print(contents);
+        System.out.print("You emptied the bin so now theres" + contents);
+
+    }
+
+    public void shake(){
+        System.out.print("You shook the bin");
+    }
+
+    public void open(){
+        System.out.print("You opened the bin");
+    }
+    
+    //store
+    //empty
+    //shake
+    //open
+}
---
+++ day0/BinTester.java
@@ -0,0 +1,9 @@
+public class BinTester {
+    public static void main(String[] args){
+        System.out.print("hello");
+
+        Bin period1Bin = new Bin();
+        period1Bin.shake();
+        period1Bin.empty();
+    }
+}
