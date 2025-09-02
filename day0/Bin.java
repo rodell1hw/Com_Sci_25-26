3768286b0515c64134414265cc09db31d13177d0
rodell1hw <rodell1@hwemail.com>
2025-09-02 11:45:36 -0700

    Update Bin.java

--- day0/Bin.java
+++ day0/Bin.java
@@ -2,7 +2,7 @@ public class Bin {
 
     private double heightft = 2.4;
     private String shape = "rectangular";
-    private boolean canOpen = true;
+    private boolean isCLosed = true;
     private String contents = "plastic";
     private int holes = 1;
     private char initial = 'B';
@@ -29,18 +29,24 @@ public class Bin {
 
     public void empty(){
         contents = "nothing";
-        System.out.print("You emptied the bin");
+        System.out.print("You emptied the bin ");
         System.out.print(contents);
-        System.out.print("You emptied the bin so now theres" + contents);
+        System.out.print("You emptied the bin so now theres " + contents);
 
     }
 
     public void shake(){
-        System.out.print("You shook the bin");
+        System.out.print("You shook the bin ");
     }
 
-    public void open(){
-        System.out.print("You opened the bin");
+    public void open(boolean isClosed){
+        if (isCLosed == true) {
+            System.out.println("You opened the bin ");
+            isClosed = false;
+        } else {
+            System.out.println("Bin is already open ");
+        }
+
     }
     
     //store
