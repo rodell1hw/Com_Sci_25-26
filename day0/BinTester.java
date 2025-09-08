312ad09c16a015bea0ca5d7b118ea2a37fb69592
rodell1hw <rodell1@hwemail.com>
2025-09-08 14:55:05 -0700

    9/8/2025

--- day0/BinTester.java
+++ day0/BinTester.java
@@ -1,10 +1,14 @@
 public class BinTester {
     public static void main(String[] args){
-        System.out.print("hello");
+        System.out.print("hello ");
 
         Bin period1Bin = new Bin();
-        period1Bin.shake();
-        period1Bin.empty();
-        period1Bin.open(true);
+        Bin period1Bin2 = new Bin(3);
+
+        // period1Bin.shake();
+        // period1Bin.empty();
+        // period1Bin.open(true);
+        System.out.println(period1Bin.toString());
+        System.out.println(period1Bin2.toString());
     }
 }
